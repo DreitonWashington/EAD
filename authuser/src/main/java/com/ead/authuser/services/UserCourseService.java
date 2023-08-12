ce24341ce6f540b32a629636631060499e1d59cd
Dreiton Washington <dreiton.washington@gmail.com>
2023-08-12 13:41:04 -0300

    Criado saveSubscriptionUserInCourse metodo e algumas modificações para atender as necessidades do metodo

--- authuser/src/main/java/com/ead/authuser/services/UserCourseService.java
+++ authuser/src/main/java/com/ead/authuser/services/UserCourseService.java
@@ -1,6 +1,13 @@
 package com.ead.authuser.services;
 
+import com.ead.authuser.models.UserCourseModel;
+import com.ead.authuser.models.UserModel;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 public interface UserCourseService {
+    boolean existsByUserAndCourseId(UserModel userModel, UUID courseId);
+
+    UserCourseModel save(UserCourseModel userCourseModel);
 }
--- authuser/src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
@@ -1,9 +1,13 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserCourseModel;
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserCourseRespository;
 import com.ead.authuser.services.UserCourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserCourseServiceImpl implements UserCourseService {
@@ -13,5 +17,15 @@ public class UserCourseServiceImpl implements UserCourseService {
 
     public UserCourseServiceImpl(UserCourseRespository userCourseRepository) {
         this.userCourseRepository = userCourseRepository;
+    }
+
+    @Override
+    public boolean existsByUserAndCourseId(UserModel userModel, UUID courseId) {
+        return userCourseRepository.existsByUserAndCourseId(userModel, courseId);
+    }
+
+    @Override
+    public UserCourseModel save(UserCourseModel userCourseModel) {
+        return userCourseRepository.save(userCourseModel);
     }
 }
