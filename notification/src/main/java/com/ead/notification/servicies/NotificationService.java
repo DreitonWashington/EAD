3f60370af9b75c0f41c861e9c59b90aaf2bad0d8
Dreiton Washington <dreiton.washington@gmail.com>
2023-09-10 13:29:01 -0300

    created consumer and dto

--- notification/src/main/java/com/ead/notification/servicies/NotificationService.java
+++ notification/src/main/java/com/ead/notification/servicies/NotificationService.java
@@ -1,4 +1,7 @@
 package com.ead.notification.servicies;
 
+import com.ead.notification.models.NotificationModel;
+
 public interface NotificationService {
+    NotificationModel saveNotification(NotificationModel notificationModel);
 }
