c76f521f6e82869fcf710c8b0b5bf6894310acdc
Dreiton Washington <dreiton.washington@gmail.com>
2023-09-10 22:15:55 -0300

    updated

--- notification/src/main/java/com/ead/notification/repositories/NotificationRepository.java
+++ notification/src/main/java/com/ead/notification/repositories/NotificationRepository.java
@@ -5,10 +5,12 @@ import com.ead.notification.models.NotificationModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<NotificationModel, UUID> {
 
     Page<NotificationModel> findAllByUserIdAndNotificationStatus(UUID userId, NotificationStatus notificationStatus, Pageable pageable);
